36316bf592aba6815481241bf6abef1a54401c3c
zhujianhui <zjnbzjh@163.com>
2019-04-23 14:55:24 +0800

    init

---
+++ DjangoWebProject/DjangoWebProject/env2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\zjh\19_04\python\DjangoWebProject\DjangoWebProject\env2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
