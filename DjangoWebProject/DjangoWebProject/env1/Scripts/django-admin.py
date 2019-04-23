1dfc4b9b3e5b32d358c7f4081d3676191521589e
zhujianhui <zjnbzjh@163.com>
2019-04-23 14:58:32 +0800

    delete unused env

--- DjangoWebProject/DjangoWebProject/env1/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\zjh\19_04\python\DjangoWebProject\DjangoWebProject\env1\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
